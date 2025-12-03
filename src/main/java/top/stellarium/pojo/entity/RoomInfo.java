6952c6d195a1ca503f4d4cccd5fd180fca14a9c4
nnyr <1659139780@qq.com>
2025-12-03 15:10:33 +0800

    added RoomInfo

---
+++ src/main/java/top/stellarium/pojo/entity/RoomInfo.java
@@ -0,0 +1,16 @@
+package top.stellarium.pojo.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RoomInfo {
+    private Long id;
+    private String title;
+    private Long animeId;
+    private Integer currentEpisode;
+    private Long ownerId;
+    private Integer onlineCount;
+    private Integer status;
+}
